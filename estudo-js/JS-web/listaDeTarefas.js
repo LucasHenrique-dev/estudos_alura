e884cb1bb0c5d1ef93916eedd336c3fabbc96168
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2021-05-26 14:52:33 -0300

    template String + create element

---
+++ estudo-js/JS-web/listaDeTarefas.js
@@ -0,0 +1,24 @@
+const criarTarefa = (event) => {
+
+    event.preventDefault();
+
+    const lista = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const inputValue = input.value;
+
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    
+    const paragrafo = `<p class="content">${inputValue}</p>`;
+
+    tarefa.innerHTML = paragrafo;
+
+    lista.appendChild(tarefa);
+    input.value = " ";
+}
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+
+novaTarefa.addEventListener('click', criarTarefa);
+
